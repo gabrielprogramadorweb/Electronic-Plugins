47c0e065a950dcebc3d8768d9806516531b89071
gabrielprogramadorads <gabriel.programador.ads@gmail.com>
2023-01-16 18:49:12 -0300

    att

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -3,17 +3,20 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+
     if (req.method === 'POST') {
         try {
             const params = {
                 submit_type: 'pay',
                 mode: 'payment',
                 payment_method_types: ['card'],
+                shipping_address_collection: { allowed_countries: ['BR', []] },
                 billing_address_collection: 'auto',
                 shipping_options: [
                     { shipping_rate: 'shr_1MQzDhG4hblE78vEbAa6TQi9' },
                     { shipping_rate: 'shr_1MQzEIG4hblE78vEmgBk1OVw' },
                 ],
+
                 line_items: req.body.map((item) => {
                     const img = item.image[0].asset._ref;
                     const newImage = img.replace('image-', 'https://cdn.sanity.io/images/3izdcu8o/production/').replace('-webp', '.webp');
