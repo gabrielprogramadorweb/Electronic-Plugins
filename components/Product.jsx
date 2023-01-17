ab9f0f73e7c0ab138e452ad046d61b9a76223702
gabrielprogramadorads <gabriel.programador.ads@gmail.com>
2023-01-17 19:29:57 -0300

    att

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 import { urlFor } from '../lib/client'
+import css from '../styles/Product.module.css'
 
-const Product = ({ product: { image, name, slug, price } }) => {
+const Product = ({ product: { image, name, slug, price, promo } }) => {
     return (
         <div>
             <Link href={`/product/${slug.current}`}>
@@ -13,6 +14,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
                         height={420}
                         className="product-image"
                     />
+                    <p className={css.promo}>{promo}</p>
                     <p className="product-name">{name} </p>
                     <p className="product-price">R${price} </p>
                 </div>
--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -18,6 +18,11 @@ export default {
             type: 'string',
         },
         {
+            name: 'promo',
+            title: 'Promo',
+            type: 'string',
+        },
+        {
             name: 'slug',
             title: 'Slug',
             type: 'slug',
