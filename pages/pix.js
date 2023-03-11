d214d50653216648d08e139bede3110f41111d99
gabrielprogramadorads <gabriel.programador.ads@gmail.com>
2023-03-10 21:18:12 -0300

    att

--- pages/pix.js
+++ pages/pix.js
@@ -1,26 +1,27 @@
 import Image from 'next/image'
 import React from 'react'
 import Img from '../public/pix.png'
+import css from '../styles/Pix.module.css'
 
 const Pix = () => {
     return (
         <>
-            <div className="container">
-                <div className="pay">
+            <div className={css.container}>
+                <div className={css.pay}>
                     <h1>Pague com pix, leia o qr code abaixo.</h1>
 
                 </div>
 
-                <div className='img'>
+                <div className={css.img}>
                     <Image src={Img} alt="" layout="intrinsic" />
                 </div>
 
 
-                <div className="chave">
+                <div className={css.chave}>
                     <h1>Ou use a chave: 1d07070b-c6dd-4911-9c56-74027e297bcc</h1>
                 </div>
 
-                <div className="info">
+                <div className={css.info}>
                     <h1>Após pagamento, envie o comprovante para: electronicplugins@gmail.com</h1>
                 </div>
 
